17f1c1b6c8d12ede7bf00226ae02616510a741cf
korrova11 <korrova@yandex.ru>
2023-07-11 02:56:01 +0300

    начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+    var dog = 8.0;
+    var cat = 3.6;
+    var paper = 763789;
+    System.out.println(dog);
+    System.out.println(cat);
+    System.out.println(paper);
+}
+public static void task2 () {
+        System.out.println("Задача 2");
+        var dog=8;
+        dog=dog+4;
+        System.out.println(dog);
+        var cat=3.6;
+        cat=cat+4;
+        System.out.println(cat);
+        var paper=763789;
+        paper=paper+4;
+        System.out.println(paper);
+
+}
+public static void task3 () {
+        System.out.println("Задача 3");
+        var dog=8.0;
+        dog=dog-3.5;
+        System.out.println(dog);
+        var cat=3.6;
+        cat=cat-1.6;
+        System.out.println(cat);
+        var paper=763789;
+        paper=paper-7639;
+        System.out.println(paper);
+}
+public static void task4 () {
+        System.out.println("Задача 4");
+        var friend=19;
+        System.out.println(friend);
+        friend=friend*2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+}
+public static void task5 () {
+        System.out.println("задача 5");
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+}
+public static void task6 () {
+        System.out.println("задача 6");
+        var boks1=78.2;
+        var boks2=82.7;
+        System.out.println(boks1+boks2);
+        System.out.println(boks2-boks1);
+}
+public static void task7 () {
+    System.out.println("задача 7");
+    var boks1 = 78.2;
+    var boks2 = 82.7;
+    System.out.println(boks2 - boks1);
+    System.out.println(boks2 % boks1);
+}
+public static void task8 () {
+    System.out.println("задача 8");
+    var hon = 640.0;
+
+
+   var job=hon/8;
+    System.out.println("Всего работников в компании " + job + " человек");
+    job = job + 94;
+    var honor = job * 8;
+    System.out.println("Если в компании работает " + job + "человек, то всего " + honor + " часов работы может быть поделено между сотрудниками");
+}}
+
